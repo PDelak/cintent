25eab7af968fa236b7c8120a27a5d4498ef3e9a4
Delewski <przemyslaw.delewski@gmail.com>
2017-08-04 21:05:27 +0200

    cintent skeleton

--- tools/driver/cintent.cpp
+++ tools/driver/cintent.cpp
@@ -1,9 +1,29 @@
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/ManagedStatic.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include "cling/Interpreter/Interpreter.h"
 
 int main(int argc, char** argv) {
-  const char* LLVMRESDIR = "/usr/local/"; //path to llvm resource directory
-  cling::Interpreter interp(argc, argv, LLVMRESDIR);
+  llvm::llvm_shutdown_obj shutdownTrigger;
 
-  interp.declare("int p=0;");
+  llvm::sys::PrintStackTraceOnErrorSignal(argv[0]);
+  llvm::PrettyStackTraceProgram X(argc, argv);
+
+  cling::Interpreter interp(argc, argv);
+
+  const cling::InvocationOptions& Opts = interp.getOptions();
+
+  for (const std::string& Lib : Opts.LibsToLoad)
+    interp.loadFile(Lib);
+
+  interp.AddIncludePath(".");
+  interp.process("#include <cstdio>");
+  interp.process("printf(\"cintent a tool for metaprogramming\")");
+
+  return 0;
 }
-        
