c8cdb4f66f2fb701d388984fd4c1cce54ef91316
Delewski <przemyslaw.delewski@gmail.com>
2017-08-20 20:33:24 +0200

    runtime simplified

--- tools/driver/cintentrt.cpp
+++ tools/driver/cintentrt.cpp
@@ -57,55 +57,28 @@ void setupCompilerInstance(CompilerInstance& TheCompInst)
 void addIncludeDirectory(CompilerInstance& TheCompInst, const std::string& includeDirectory)
 {
      TheCompInst.getHeaderSearchOpts().AddPath(includeDirectory, frontend::Angled, false, false);
- 
-}
 
-llvm::cl::opt<std::string> InputFilename(llvm::cl::Positional, llvm::cl::Required, llvm::cl::desc("<input file>"));
-llvm::cl::opt<std::string> Inject("inject", llvm::cl::desc("Specify config filename"), llvm::cl::value_desc("config"));
-llvm::cl::opt<std::string> Runtime("runtime", llvm::cl::desc("Specify runtime"), llvm::cl::value_desc("runtime"));
-llvm::cl::opt<bool> ReportFunctions("reportFunctions", llvm::cl::desc("Report all found functions"));
-llvm::cl::list<std::string> Include("I", llvm::cl::desc("Specify include directory"), llvm::cl::value_desc("include directory"));
-
-
-
-void HideNotNeededOptions(llvm::StringMap<llvm::cl::Option*> &Map)
-{
-    using namespace llvm;
-    Map["rng-seed"]->setHiddenFlag(cl::Hidden);
-    Map["static-func-full-module-prefix"]->setHiddenFlag(cl::Hidden);
-    Map["stats"]->setHiddenFlag(cl::Hidden);
-    Map["stats-json"]->setHiddenFlag(cl::Hidden);
-    Map["version"]->setHiddenFlag(cl::Hidden);
 }
 
 int cintentrt() { return 0; }
 
-int exec(const std::string& inFileName, Rewriter& TheRewriter, ASTConsumer& TheConsumer)
+int exec(const std::string& inputFilename, Rewriter& TheRewriter, ASTConsumer& TheConsumer)
 {
   using namespace llvm;
-  HideNotNeededOptions(cl::getRegisteredOptions());
 
-  InputFilename = inFileName;
-
-  if (!std::ifstream(InputFilename).is_open()) {
+  if (!std::ifstream(inputFilename).is_open()) {
       llvm::errs() << "File does not exists";
       return -1;
   }
 
   CompilerInstance TheCompInst;
 
-  int num = Include.getNumOccurrences();
-
-  for (int i = 0; i < num; ++i) {
-      addIncludeDirectory(TheCompInst, Include[i]);
-  }
-
   setupCompilerInstance(TheCompInst);
 
   FileManager &FileMgr = TheCompInst.getFileManager();
   SourceManager &SourceMgr = TheCompInst.getSourceManager();
 
-  const FileEntry *FileIn = FileMgr.getFile(InputFilename);
+  const FileEntry *FileIn = FileMgr.getFile(inputFilename);
   clang::FileID fileId = SourceMgr.createFileID(FileIn, SourceLocation(), SrcMgr::C_User);
 
   SourceMgr.setMainFileID(fileId);
@@ -122,22 +95,24 @@ int exec(const std::string& inFileName, Rewriter& TheRewriter, ASTConsumer& TheC
   const RewriteBuffer *RewriteBuf = TheRewriter.getRewriteBufferFor(SourceMgr.getMainFileID());
   if (!RewriteBuf) return -1;
 
-  std::string oldFile = InputFilename + ".old";
-  rename(InputFilename.c_str(), oldFile.c_str());
+  std::string oldFile = inputFilename + ".old";
+  rename(inputFilename.c_str(), oldFile.c_str());
 
   std::string outStr;
 
   outStr.append(std::string(RewriteBuf->begin(), RewriteBuf->end()));
 
-  std::ofstream out(InputFilename);
+  std::ofstream out(inputFilename);
 
   std::error_code EC;
-  std::unique_ptr<llvm::raw_fd_ostream> o (new raw_fd_ostream(InputFilename, EC, sys::fs::F_None));
+  std::unique_ptr<llvm::raw_fd_ostream> o (new raw_fd_ostream(inputFilename, EC, sys::fs::F_None));
   o->SetUnbuffered();
   o->write(outStr.c_str(), outStr.size());
 
 
   return 0;
 }
+
+
 
                                          
